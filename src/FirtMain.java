bfde90787e5e03eea52d8214c4a1a02af71d34ba
Till Sauerwein <till.sauerwein@Tills-MacBook-Pro.local>
2021-09-07 16:20:51 +0200

    added FirstMain

---
+++ src/FirtMain.java
@@ -0,0 +1,8 @@
+public class FirtMain {
+    public static void main(String[] args){
+        int a = 2;
+        int b = 4;
+        int summe = a + b;
+        System.out.println("das die summe aus a und b ist: " + summe);
+    }
+}
